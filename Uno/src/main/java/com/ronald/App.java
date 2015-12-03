d8b244863e12db66a255cf052a973d0ad911b89a
Ronald331 <Ronald.dev1@gmail.com>
2015-12-03 15:37:47 -0500

    Fix error's

--- Uno/src/main/java/com/ronald/App.java
+++ Uno/src/main/java/com/ronald/App.java
@@ -8,24 +8,27 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public static void main(String[] args) throws Exception
+public class App
 {
-    Configuration conf = new Configuration();
-    Job job = new Job(conf);
-    job.setJarByClass(App.class);
-    job.setJobName("MapRed - Ronald");
-    job.setMapperClass(AppMapper.class);
-    job.setReducerClass(AppReducer.class);
-    
-    FileInputFormat.setInputPaths(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-    
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
-    
-    job.setMapOutputKeyClass(Text.class);
-    job.setMapOutputValueClass(IntWritable.class);
-    
-    if(job.waitForCompletion(true)) { System.exit(0); }
-    else { System.exit(1); }
+    public static void main(String[] args) throws Exception
+    {
+        Configuration conf = new Configuration();
+        Job job = new Job(conf);
+        job.setJarByClass(App.class);
+        job.setJobName("MapRed - Ronald");
+        job.setMapperClass(AppMapper.class);
+        job.setReducerClass(AppReducer.class);
+        
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        
+        if(job.waitForCompletion(true)) { System.exit(0); }
+        else { System.exit(1); }
+    }
 }
